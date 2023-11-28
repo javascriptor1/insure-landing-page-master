6d92e3592dca95c666e2d7ee6b9512230e3aa360
Mohammed Fakih <mhd.fakih@gmail.com>
2023-11-28 03:36:02 +0300

    mobile menu open/close done

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const hamburgerClose = document.querySelector(".hamburger-close");
+const mobileMenu = document.querySelector(".header__nav");
+
+hamburgerMenu.addEventListener("click", openMenu);
+hamburgerClose.addEventListener("click", closeMenu);
+
+function openMenu() {
+  hamburgerMenu.style.display = "none";
+  hamburgerClose.style.display = "block";
+  mobileMenu.style.display = "block";
+}
+function closeMenu() {
+  hamburgerClose.style.display = "none";
+  hamburgerMenu.style.display = "block";
+  mobileMenu.style.display = "none";
+}
